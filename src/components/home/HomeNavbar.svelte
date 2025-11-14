5ebaefe478575d2b3597c02a2b4add79e9a315f8
Iwansal64 <iwansal64@gmail.com>
2025-11-14 09:20:02 +0700

    [improve] Floating navbar design

--- src/components/home/HomeNavbar.svelte
+++ src/components/home/HomeNavbar.svelte
@@ -1,4 +1,4 @@
-<nav class="w-[calc(100vw-32px)] h-16 py-4 px-4 flex flex-row gap-4 justify-between items-center fixed top-4 left-1/2 -translate-x-1/2 bg-black rounded-full">
+<nav class="w-[90vw] max-w-[1000px] h-16 py-4 px-4 flex flex-row gap-4 justify-between items-center fixed top-4 left-1/2 -translate-x-1/2 bg-black rounded-full">
       <div class="h-full flex flex-col justify-center px-8">
             <h1 class="w-fit text-2xl font-bold">KPRS</h1>
             <p class="-mt-2 w-fit text-sm font-thin">Komite Pemilihan Raya Siswa</p>
