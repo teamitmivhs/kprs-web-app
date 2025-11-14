bd83daad893ced7d3e602aaf44b288a6304fd989
Iwansal64 <iwansal64@gmail.com>
2025-11-14 14:04:13 +0700

    [new] Add wayang image

---
+++ src/components/home/HomeWayang.svelte
@@ -0,0 +1,7 @@
+<script>
+      import Wayang1 from "../../assets/wayang1.png";
+</script>
+
+<div class="absolute w-dvw h-dvh *:absolute *:w-96 opacity-50 pointer-events-none">
+      <img class="top-24 right-24 -rotate-15" src={Wayang1} alt="Wayang 1">
+</div>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,7 +1,8 @@
 <script>
       import HomeNavbar from "../components/home/HomeNavbar.svelte";
-
+      import HomeWayang from "../components/home/HomeWayang.svelte";
 
 </script>
 
 <HomeNavbar />
+<HomeWayang />
