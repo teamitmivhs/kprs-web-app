93ee54a5f5c496e2e1030facbe0ee9a3a42f7ff3
Iwansal64 <iwansal64@gmail.com>
2025-12-14 12:36:29 +0700

    [improve] Change from a button to a href link

--- src/components/home/HomeNavbar.svelte
+++ src/components/home/HomeNavbar.svelte
@@ -1,9 +1,3 @@
-<script>
-      function handleVoteClick() {
-            window.location.hash = '/gate';
-      }
-</script>
-
 <nav class="w-[90vw] max-w-[1000px] h-18 py-4 px-4 flex flex-row gap-4 justify-between items-center fixed top-4 left-1/2 -translate-x-1/2 bg-[#8a7143] rounded-xl z-100">
       <div class="h-full flex flex-col justify-center px-8 w-48 -space-y-1">
             <a href="./#" class="w-fit text-2xl font-bold duration-100 hover:scale-110">KPRS</a>
@@ -14,7 +8,10 @@
                   <a class="satisfying-button p-2 rounded-full navbar-link" href="#tutorial">Tutorial</a>
             </div>
       </div>
-      <button onclick={handleVoteClick} class="h-full bg-white text-[#8a7143] rounded-full px-6 text-xl font-semibold border border-transparent cursor-pointer duration-150 satisfying-button">
+      <a
+            href="/#/gate"
+            class="h-full flex justify-center items-center bg-white text-[#8a7143] rounded-full px-6 text-xl font-semibold border border-transparent cursor-pointer duration-150 satisfying-button"
+      >
             Vote
-      </button>
+      </a>
 </nav>
